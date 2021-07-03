f37aa21bf6660997d81ce778d6d258e49417ac99
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-07-03 20:08:11 +0700

    Cleanup bluetooth

--- src/cleanup.sh
+++ src/cleanup.sh
@@ -4,10 +4,17 @@ rm -Rf autom4te.cache m4 config aclocal.m4 compile config.* \
       configure depcomp install-sh ltmain.sh Makefile.in missing \
       stamp-h1 Makefile libtool build \
       jscturret/Makefile.in jscturret/.deps jscturret/Makefile \
-      btservice/Makefile.in btservice/Makefile btservice/.deps \
-      btservice/.libs btservice/libbtservice_la* btservice/libbtservice.la \
+      jsc-bluetooth/src/btservice/Makefile.in \
+      jsc-bluetooth/src/btservice/Makefile \
+      jsc-bluetooth/src/btservice/.deps \
+      jsc-bluetooth/src/btservice/.libs \
+      jsc-bluetooth/src/btservice/libbtservice_la* \
+      jsc-bluetooth/src/btservice/libbtservice.la \
       stepper/Makefile.in stepper/Makefile stepper/.deps \
       stepper/.libs stepper/libstepper_la* stepper/libstepper.la \
-      utils/Makefile.in utils/Makefile utils/.deps \
-      utils/.libs utils/libutils_la* utils/libutils.la
+      jsc-bluetooth/src/utils/Makefile.in \
+      jsc-bluetooth/src/utils/Makefile \
+      jsc-bluetooth/src/utils/.deps \
+      jsc-bluetooth/src/utils/.libs utils/libutils_la* \
+      jsc-bluetooth/src/utils/libutils.la
       
