d4e4c13450480c55b47e17ec464f53837170fbd5
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-07-03 22:46:47 +0700

    Enable turret center position.

--- src/jscturret/jscturret.c
+++ src/jscturret/jscturret.c
@@ -4,18 +4,24 @@
 #include "btcommon.h"
 #include "btserver.h"
 #include "jscturret.h"
+#include "stepper.h"
 #include "logger.h"
 
 void messageCallback(char *message)
 {
-  g_log(JSCBT, G_LOG_LEVEL_MESSAGE, "%s", message);
+  if (!strcmp(message, "center"))
+  {
+    centerTurret(SLOW);
+  }
 }
 
-int main(int argc, char const *argv[])
+void init_stepper()
 {
-  GLogLevelFlags flag = G_LOG_LEVEL_ERROR | G_LOG_LEVEL_WARNING | G_LOG_LEVEL_MESSAGE | G_LOG_LEVEL_CRITICAL;
-  g_log(JSCBT, G_LOG_LEVEL_MESSAGE, "Starting service \"%s\".", BLUETOOTH_SERVICE_NAME);
+  setupStepper();
+}
 
+void init_bluetooth(GLogLevelFlags flag)
+{
   serverConnectionData *data = malloc(sizeof(serverConnectionData));
   data->service_channel = BLUETOOTH_SERVICE_CHANNEL;
   data->service_name = BLUETOOTH_SERVICE_NAME;
@@ -23,9 +29,17 @@ int main(int argc, char const *argv[])
   data->service_uuid = BLUETOOTH_SERVICE_UUID;
   data->messageCallback = messageCallback;
 
+  g_log(JSCBT, G_LOG_LEVEL_MESSAGE, "Starting service \"%s\".", BLUETOOTH_SERVICE_NAME);
   int result = register_service(data, flag);
   if (result != RESULT_OK)
   {
     g_log(JSCBT, G_LOG_LEVEL_ERROR, "Error %d occured while registering.", result);
   }
+}
+
+int main(int argc, char const *argv[])
+{
+  GLogLevelFlags flag = G_LOG_LEVEL_ERROR | G_LOG_LEVEL_WARNING | G_LOG_LEVEL_MESSAGE | G_LOG_LEVEL_CRITICAL;
+
+  init_bluetooth(flag);
 }
